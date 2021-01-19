c89713ff49727260487408aeb1458b14817fa989
sharozali <cityuni06@gmail.com>
2021-01-20 00:10:44 +0500

    update sections

--- src/Views/LandingPages/Components/index.jsx
+++ src/Views/LandingPages/Components/index.jsx
@@ -4,6 +4,8 @@ export default function Index(props) {
     return <Fragment>
         <GetStarted />
         <WillSection />
+        <Forms />
+        <LastWill />
     </Fragment>
 }
 
@@ -87,6 +89,129 @@ function WillSection(props) {
 
                 </div>
             </div>
+        </div>
+    </section>
+}
+
+function Forms(props) {
+    return <section class="forms-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="col-lg-2 col-md-2 col-sm-2"></div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 witnesa-page-form">
+                        <img src="./dist/assets/img/form1.png" alt="" />
+                        <span>Witnesa page form</span>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 self-affidevit-form">
+                        <img src="./dist/assets/img/form2.png" alt="" />
+                        <span>Self affidevit form</span>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+}
+
+function LastWill(props) {
+    return <section class="last-will-section">
+        <div class="upper-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <div class="col-lg-5 col-md-5 col-sm-6 last-will">
+                            <div class="heading-border">
+                                <span class="heading"> HOW US WILL REGISTRAY WORKS</span>
+                            </div>
+
+                            <p class="last-will-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim sequi voluptate distinctio laboriosam vero iste debitis quidem architecto, repudiandae modi, animi minus facere fugiat exercitationem quasi magnam quibusdam expedita?</p>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="col-sm-3">
+                                        <span class="count">1</span>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <span class="mini-heading"> FILL OUT FORM</span>
+                                        <p class="mini-para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae maiores nihil dolores neque possimus ad magnam itaque labore.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="col-sm-3">
+                                        <span class="count">1</span>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <span class="mini-heading"> FILL OUT FORM</span>
+                                        <p class="mini-para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae maiores nihil dolores neque possimus ad magnam itaque labore.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="col-sm-3">
+                                        <span class="count">1</span>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <span class="mini-heading"> FILL OUT FORM</span>
+                                        <p class="mini-para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae maiores nihil dolores neque possimus ad magnam itaque labore.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="col-sm-3">
+                                        <span class="count">1</span>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <span class="mini-heading"> FILL OUT FORM</span>
+                                        <p class="mini-para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae maiores nihil dolores neque possimus ad magnam itaque labore.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-7 col-md-7 col-sm-6 last-will-img">
+                            <img src="./dist/assets/img/last-will.png" alt="last will" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="lower-section">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="col-sm-4"></div>
+                    <div class="col-sm-4">
+                        <div class="info-border">
+                            <span class="info-heading">THE U.S. WILL REGISTRAY OFFERS</span>
+                        </div>
+                    </div>
+                    <div class="col-sm-4"></div>
+                </div>
+
+                <p class="Nationwide">Nationwide Search for a Missing Last Will</p>
+                <p class="info-detail">Lifetime registration of Last Will</p>
+                <div class="col-sm-12">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p class="info-mini-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur accusantium aspernatur similique maxime omnis.Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-8">
+                        <p class="info-mini-detail-b">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias sed cum accusamus doloremque esse! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque eius earum beatae suscipit delectus, debitis ab, totam mollitia magni velit sed consequuntur itaque illum.</p>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="make-your-will">
+            <img src="./dist/assets/img/make-will.png" alt="make your will" />
+            <button>GET STARTED</button>
         </div>
     </section>
 }
