334dbc015eba13ca131fe0bbc1cf82b0ed52d15f
sharozali <cityuni06@gmail.com>
2021-01-20 00:38:39 +0500

    add header and footer

---
+++ src/Views/Footer/footer.jsx
@@ -0,0 +1,104 @@
+import { Fragment } from "react";
+
+export default function Footer(props) {
+    return <Fragment>
+        <footer class="footer spad">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-4">
+                        <div class="footer__about">
+                            <div class="footer__about__logo">
+                                <img src="./dist/assets/img/footer-logo1.png" alt="logo">
+                          </div>
+                                <div class="footer__widget">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt cupiditate soluta illum delectus, deserunt, commodi dignissimos molestiae itaque magni voluptate officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt cupiditate soluta illum delectus, deserunt, commodi dignissimos molestiae itaque magni voluptate officiis.</p>
+                                </div>
+                                <ul class="list-group list-group-horizontal">
+                                    <li class="list-group-item footer-store-logo">
+                                        <img src="./dist/assets/img/twitter.png" alt="img1">
+                              </i>
+                                        <li class="list-group-item footer-store-logo">
+                                            <img src="./dist/assets/img/fb.png" alt="img1">
+                              </i>
+                                            <li class="list-group-item footer-store-logo">
+                                                <img src="./dist/assets/img/google.png" alt="img1">
+                              </i>
+                                                <li class="list-group-item footer-store-logo">
+                                                    <img src="./dist/assets/img/insta.png" alt="img1">
+                              </i>
+                              
+                            </ul>
+                      </div>
+                  </div>
+                                        <div class="col-lg-8 col-md-8 col-sm-8 footer-list-div">
+                                            <div class="row">
+                                                <div class="col-lg-3 col-md-3 col-sm-6">
+                                                    <div class="footer__widget">
+                                                        <h5 class="footer-heading">OUR SERVICES</h5>
+                                                        <ul>
+                                                            <li><a href="#">About</a></li>
+                                                            <li><a href="#">Careers</a></li>
+                                                            <li><a href="#">Security</a></li>
+                                                            <li><a href="#">Contact Us</a></li>
+                                                            <li><a href="#">Press</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3 col-md-3 col-sm-6">
+                                                    <div class="footer__widget">
+                                                        <h5 class="footer-heading">EXPLORE</h5>
+                                                        <ul>
+                                                            <li><a href="#">Our Products</a></li>
+                                                            <li><a href="#">Blog</a></li>
+                                                            <li><a href="#">Glossary</a></li>
+                                                            <li><a href="#">Help Center</a></li>
+                                                            <li><a href="#">Community forum</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3 col-md-3 col-sm-6">
+                                                    <div class="footer__widget">
+                                                        <h5 class="footer-heading">LEGAL </h5>
+                                                        <ul>
+                                                            <li><a href="#">Terms & Conditions</a></li>
+                                                            <li><a href="#">Privacy Notice</a></li>
+                                                        </ul>
+
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-3 col-md-3 col-sm-6">
+                                                    <div class="footer__widget">
+                                                        <h5 class="footer-heading">FOR NONPROFITS</h5>
+                                                        <ul>
+                                                            <li><a href="#">For nonprofits</a></li>
+                                                            <li><a href="#">Request a demo</a></li>
+                                                            <li><a href="#">Resources</a></li>
+                                                            <li><a href="#">Fundraising during</a></li>
+                                                            <li><a href="#">Coronavirus</a></li>
+                                                        </ul>
+
+                                                    </div>
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+                  
+              </div>
+          </div>
+  
+          
+      </footer>
+                            <footer style="background-color: #1a1919;">
+                                <div class="container">
+                                    <div class="row">
+
+                                        <div class="col-md-12 copyrights">
+                                            <p>Copyright &copy;2019-2020 US Will Registry Co, a Delaware Public Benifits Corporation. All rights reserved </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </footer>
+    </Fragment>
+}
---
+++ src/Views/Header/header.jsx
@@ -0,0 +1,56 @@
+export default function Header(props) {
+    return <section class="header-section">
+        <div class="header-top-part">
+            <div class="container">
+                <div class="upper-header">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+                            <img src="./dist/assets/img/header-logo-a.png" alt="logo" class="logo-img" />
+                        </div>
+                        <div class="col-lg-8 col-md-8 col-sm-6">
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-lower-part">
+            <div class="container">
+                <div class="lower-header">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <a href="#" class="top-menu active-menu">Home</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#" class="top-menu">Contact us Pages</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#" class="top-menu">About Us</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#" class="top-menu">Site Map</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#" class="top-menu">Dictionary</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#" class="top-menu">Terms of Service</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#" class="top-menu">Privacy Notice</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#" class="top-menu">For Non Profit</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#" class="top-menu">FAQ</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="header-right-img">
+            <img src="./dist/assets/img/header-logo.png" alt="" />
+        </div>
+    </section>
+}
