d2583460fd77de3068c720ac8d792da4afd1ce61
usman <cityuni06@gmail.com>
2021-03-24 16:14:24 +0500

    add validate email funciton

--- src/Views/Profile/Components/editProfile.jsx
+++ src/Views/Profile/Components/editProfile.jsx
@@ -212,6 +212,10 @@ function EditProfile(props) {
         }
         return isValid;
     }
+    const validateEmail = (email) => {
+		const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(String(email).toLowerCase());
+	}
     return <React.Fragment>
         <Style />
         <section className="profile-section">
