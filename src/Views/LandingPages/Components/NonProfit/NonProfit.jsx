31b38972d2bebfc2750acb81d1a67e090dbdae6c
SajidLakhani <sajidlakhani59@gmail.com>
2021-03-26 19:26:19 +0500

    Setup of non profit page

--- src/Views/LandingPages/Components/NonProfit/NonProfit.jsx
+++ src/Views/LandingPages/Components/NonProfit/NonProfit.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import '../../Css/nonprofit.css'
 const NonProfit = () => {
     return (
-        <div>
+        <div className = "name">
             Sajid
         </div>
     );
